1333ee66aca8c414ca966d27e1e5bf1e1b5ec1d0
Ilia Grigorev <giv13@bk.ru>
2025-09-12 18:14:23 +0500

    Установка Spring Boot для backend части

---
+++ backend/src/main/java/ru/giv13/tasktracker/BackendApplication.java
@@ -0,0 +1,13 @@
+package ru.giv13.tasktracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ru.giv13.tasktracker.BackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/ru/giv13/tasktracker/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.giv13.tasktracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
